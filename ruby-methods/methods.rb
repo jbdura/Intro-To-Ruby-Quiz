4b6164ec3ebace5d9dc41a7d10e0034ed4519716
Johnson Bidura <99818207+jbdura@users.noreply.github.com>
2022-08-26 16:30:52 +0300

    Update methods.rb

--- ruby-methods/methods.rb
+++ ruby-methods/methods.rb
@@ -2,7 +2,6 @@
 ## Q1 Write any simple Ruby Program to illustrate the defining and calling of method
 
 ## Q2 You have a string, "I love espresso", return the length of the string
-  # puts "I love espresso".length
 
 ## Q3 Write a method where var1 and var2 are the parameters. Call the method. The statements to be executed are:
 #"Phase-1 was all about Introduction to Programming."
