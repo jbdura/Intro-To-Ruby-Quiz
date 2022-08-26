e5f5129fc164312445b2bb2c65ab153046a52a81
Jackline Jebet <jebetjackline2016@gmail.com>
2022-08-26 16:23:04 +0300

    done

--- ruby-methods/methods.rb
+++ ruby-methods/methods.rb
@@ -2,7 +2,7 @@
 ## Q1 Write any simple Ruby Program to illustrate the defining and calling of method
 
 ## Q2 You have a string, "I love espresso", return the length of the string
-  puts "I love espresso".length
+  # puts "I love espresso".length
 
 ## Q3 Write a method where var1 and var2 are the parameters. Call the method. The statements to be executed are:
 #"Phase-1 was all about Introduction to Programming."
@@ -10,6 +10,20 @@
 
 ##Q4 Write a Ruby program which accept the user's first and last name and print them in reverse order with a 
 #space between them.
+
+def name_method()
+puts " First Name: "
+fname = gets.chomp
+
+puts " Last Name: "
+lname = gets.chomp
+
+names = "#{fname} #{lname}"
+puts names
+puts names.reverse
+end
+
+
 
 ## Q5 Ruby program to illustrate the passing of parameters to a method
 # Output 
